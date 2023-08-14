58210325438ad2cced2258333005ed89172d8b8c
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-08-14 13:19:11 +0200

    Modified DateEntry

--- UI/widgets/date_entry.py
+++ UI/widgets/date_entry.py
@@ -6,14 +6,22 @@ from PIL import Image
 
 class DateEntry(customtkinter.CTkFrame):
 
-    def __init__(self, parent, placeholder_text="", font=None, width=140, **kwargs):
-        super().__init__(parent, width=width, **kwargs)
+    def __init__(self, parent, placeholder_text="", font=None, width=140, height=28, **kwargs):
+        super().__init__(parent, width=width, height=height, **kwargs)
 
-        self.entry = customtkinter.CTkEntry(self, placeholder_text=placeholder_text, font=font)
-        self.entry.grid(row=0, column=0, padx=(3, 5), pady=3)
+        self.configure(fg_color="transparent")
 
-        self.date_picker = customtkinter.CTkButton(self, command=self.command_callback, text="", width=30, image=customtkinter.CTkImage(light_image=Image.open("UI/assets/date_picker.png")))
-        self.date_picker.grid(row=0, column=1, padx=(0, 3), pady=3)
+        self.grid_columnconfigure(1, weight=0)  # buttons don't expand
+        self.grid_columnconfigure(0, weight=1)  # entry expands
+
+        self.entry = customtkinter.CTkEntry(self, placeholder_text=placeholder_text, width=width-36-5, height=height, font=font)
+        self.entry.grid(row=0, column=0, padx=(0, 5), pady=0)
+
+        button_size = height
+        self.date_picker = customtkinter.CTkButton(self, command=self.command_callback, text="", width=button_size, height=button_size, image=customtkinter.CTkImage(light_image=Image.open("UI/assets/date_picker.png"), size=(20, 20)))
+        self.date_picker.grid(row=0, column=1, padx=(0, 0), pady=0)
+
+        print(self.date_picker.cget("width"))
 
     def command_callback(self):
         date = DatePicker.get_date()
