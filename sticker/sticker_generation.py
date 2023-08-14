74f11313a799e38be81f0a876eeebc7cdc22ac1e
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-08-14 16:23:06 +0200

    refactored sticker_generation.py

--- sticker/sticker_generation.py
+++ sticker/sticker_generation.py
@@ -1,10 +1,8 @@
 import os
 import time
 
-from threading import Thread
 from datetime import datetime
 
-from CTkMessagebox import CTkMessagebox
 from PIL import ImageFont, ImageDraw, Image as PILImage
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image
