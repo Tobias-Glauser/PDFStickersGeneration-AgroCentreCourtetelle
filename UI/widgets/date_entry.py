8c53915f9de01c7792f4de7f79394c91e346c2d9
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-08-14 16:26:37 +0200

    refactored date_entry.py

--- UI/widgets/date_entry.py
+++ UI/widgets/date_entry.py
@@ -14,11 +14,23 @@ class DateEntry(customtkinter.CTkFrame):
         self.grid_columnconfigure(1, weight=0)  # buttons don't expand
         self.grid_columnconfigure(0, weight=1)  # entry expands
 
-        self.entry = customtkinter.CTkEntry(self, placeholder_text=placeholder_text, width=width-36-5, height=height, font=font)
+        self.entry = customtkinter.CTkEntry(self,
+                                            placeholder_text=placeholder_text,
+                                            width=width - 36 - 5,
+                                            height=height,
+                                            font=font)
         self.entry.grid(row=0, column=0, padx=(0, 5), pady=0)
 
         button_size = height
-        self.date_picker = customtkinter.CTkButton(self, command=self.command_callback, text="", width=button_size, height=button_size, image=customtkinter.CTkImage(light_image=Image.open("UI/assets/date_picker.png"), size=(20, 20)))
+        self.date_picker = customtkinter.CTkButton(self,
+                                                   command=self.command_callback,
+                                                   text="",
+                                                   width=button_size,
+                                                   height=button_size,
+                                                   image=customtkinter.CTkImage(light_image=Image.open("UI/assets"
+                                                                                                       "/date_picker"
+                                                                                                       ".png"),
+                                                                                size=(20, 20)))
         self.date_picker.grid(row=0, column=1, padx=(0, 0), pady=0)
 
     def command_callback(self):
@@ -29,10 +41,3 @@ class DateEntry(customtkinter.CTkFrame):
 
     def get(self):
         return self.entry.get()
-
-
-
-
-
-
-
