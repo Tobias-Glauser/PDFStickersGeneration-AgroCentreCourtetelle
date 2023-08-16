71ad3dbe59035bdf2d21df9d3187cbff4d569a98
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-08-16 11:25:32 +0200

    Comments on stickers.py

--- sticker/stickers.py
+++ sticker/stickers.py
@@ -1,5 +1,12 @@
 class Stickers:
+    """
+    Class that contains all the stickers that the user can use
+    """
     def __init__(self, stickers=None):
+        """
+        Stickers constructor
+        :param stickers: List of stickers to add to the stickers list
+        """
         if stickers is None:
             self.stickers_list = []
         else:
@@ -7,10 +14,24 @@ class Stickers:
         self.selected_sticker = None
 
     def get_stickers_values(self):
+        """
+        Get all the stickers names
+        :return: A list of all the stickers names
+        """
         return [str(sticker.name) for sticker in self.stickers_list]
 
     def add_sticker(self, sticker):
+        """
+        Add a sticker to the stickers list
+        :param sticker: Sticker to add
+        :return: None
+        """
         self.stickers_list.append(sticker)
 
     def remove_sticker(self, sticker):
+        """
+        Remove a sticker from the stickers list
+        :param sticker: Sticker to remove
+        :return: None
+        """
         self.stickers_list.remove(sticker)
